32833da68748bff7c99415169a88e669fb4d669a
Larrrisa <97353756+Larrrisa@users.noreply.github.com>
2023-11-11 23:17:57 +0400

    basic logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Button } from "semantic-ui-react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Page from "./news";
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  const getNews = async () => {
+    try {
+      const link = "https://hacker-news.firebaseio.com/v0/topstories.json";
+      const newsResult = await fetch(link);
+      const data = await newsResult.json();
+      const news = data
+        .slice(0, 100)
+        .map((id) =>
+          fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
+            (res) => res.json()
+          )
+        );
+      const result = await Promise.all(news);
+      setNews(result);
+    } catch {
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  function handleRefreshNews() {
+    getNews();
+    console.log(news);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Hacker News</h1>
+        <Button content="Refresh" active onClick={handleRefreshNews} />
+
+        {news.map((data) => {
+          return (
+            <div key={data.id}>
+              <p>
+                <Link to="news">
+                  {data.title} - Рейтинг:{data.score} - Name: {data.by} - Date:
+                  {new Date(data.time).toString()}
+                </Link>
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
