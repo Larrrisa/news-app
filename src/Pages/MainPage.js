1e8ce8beb55801013fdb701c72404868d3d7d5da
Larrrisa <97353756+Larrrisa@users.noreply.github.com>
2023-11-23 23:57:17 +0400

    minor changes

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -47,6 +47,27 @@ export function MainPage() {
     };
   }, []);
 
+  function handleTime(item) {
+    const time = Math.floor(
+      (Date.now() - new Date(item * 1000)) / 1000 / 60 / 60
+    );
+    if (time === 0) {
+      return `${Math.floor(
+        (Date.now() - new Date(item * 1000)) / 1000 / 60
+      )} minutes ago`;
+    } else if (time >= 24 && time < 48) {
+      return "1 day ago";
+    } else if (time >= 48 && time < 72) {
+      return "2 days ago";
+    } else if (time >= 72) {
+      return "few days ago";
+    } else {
+      return `${Math.floor(
+        (Date.now() - new Date(item * 1000)) / 1000 / 60 / 60
+      )} hours ago`;
+    }
+  }
+
   return (
     <div>
       <Title>Hacker News </Title>
@@ -79,8 +100,8 @@ export function MainPage() {
                     <UserOutlined /> {data.by}
                   </div>
                   <div>
-                    <ClockCircleOutlined />{" "}
-                    {(Date.now() - new Date(data.time * 1000)) / 1000 / 60 / 60}
+                    <ClockCircleOutlined />
+                    {handleTime(data.time)}
                   </div>
                 </Flex>
               </div>
